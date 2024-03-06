7b4f78bc07b96190609a3e003356d0132f721eef
hirataka1223 <154341912+hirataka1223@users.noreply.github.com>
2024-03-07 01:59:14 +0900

    Firebase、Gitの初期設定完了

--- Firebase_test3/Firebase_test3App.swift
+++ Firebase_test3/Firebase_test3App.swift
@@ -5,13 +5,41 @@
 //  Created by Taka on 2024/03/07.
 //
 
+//import SwiftUI
+//
+//@main
+//struct Firebase_test3App: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//    }
+//}
+
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
 
 @main
 struct Firebase_test3App: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ContentView()
+            }
         }
     }
 }
